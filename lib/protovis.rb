2f1b8200a5f197233bc8eb1e0196cb2881ff474a
shaoyang <shao4phone@gmail.com>
2012-06-27 07:17:39 +0800

    Line is support .  Params can be parsed! t

--- lib/protovis.rb
+++ lib/protovis.rb
@@ -4,33 +4,43 @@ require 'erb'
 module Protovis
   Redmine::WikiFormatting::Macros.register do
              desc "This is redmine macro for protovis"
-             macro :protovis do |obj, args|
-                 objname=args[0]
-                 data=args[1]
-                 Protovis.render("#{File.dirname(__FILE__)}/#{objname}/#{objname}.html",{:data=>data})
+             macro :protovis do |obj,args|
+                 data=Protovis.parse(args)
+                 Protovis.render("#{File.dirname(__FILE__)}/#{data.objname}/#{data.objname}.html",data)
               end
            end
-  
-  def self.render filename,context
-  	 
-  	 class<<context
-  	 	def data
-          self[:data]
-  	    end
-  	 	def get_binding
+
+  def self.parse(args)  
+  	data={}
+  	data[:objname],data[:width],data[:height]=args
+  	data[:data]=args[3..-1].join(",").strip!
+  	data[:data]=data[:data].gsub("&lt;","{")
+    data[:data]=data[:data].gsub("&gt;","}")
+    data[:data]=data[:data].gsub("<br />","")
+    data[:data]=ActiveSupport::JSON.decode(data[:data])
+    data[:data]=ActiveSupport::JSON.encode(data[:data])
+  	class<<data
+  		def method_missing(method,*args,&block)
+	 	  self[method]
+	    end
+	    def get_binding
            binding
   	 	end
-  	 end
-  	 
+  	end
+    data
+  end	
+ 
+
+  def self.render filename,context
   	 begin
      content=File.open(filename,"r").read
   	 ERB.new(content,0).result(context.get_binding)
      rescue Exception => e 
-     	e.getMessage
+     	e.to_s
      end
-
-  	 
-
   end 
+
+ 
+  
 
 end
